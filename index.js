1b355dab6268f1df58f172302e6eec2a9f410943
Vinod-Jantikar <vinodjantikar999@gmail.com>
2025-01-03 20:19:33 +0530

    solve singleNumber problem

--- index.js
+++ index.js
@@ -329,3 +329,46 @@ var generate = function (numRows) {
 
   return triangle;
 };
+
+
+
+// 136. Single Number
+// Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+// You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+// Example 1:
+
+// Input: nums = [2,2,1]
+// Output: 1
+// Example 2:
+
+// Input: nums = [4,1,2,1,2]
+// Output: 4
+// Example 3:
+
+// Input: nums = [1]
+// Output: 1
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 3 * 104
+// -3 * 104 <= nums[i] <= 3 * 104
+// Each element in the array appears twice except for one element which appears only once.
+
+
+function singleNumber(nums) {
+  let result = 0;
+  for (const num of nums) {
+      result ^= num; // XOR each number
+  }
+  return result;
+}
+
+
+// Step	result (XOR so far)	Operation
+// 1	     0 ^ 4 = 4	        XOR with 4
+// 2	     4 ^ 1 = 5	        XOR with 1
+// 3	     5 ^ 2 = 7	        XOR with 2
+// 4	     7 ^ 1 = 6	        XOR with 1
+// 5	     6 ^ 2 = 4	        XOR with 2
