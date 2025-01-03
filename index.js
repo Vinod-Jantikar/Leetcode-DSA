636ce29a7dd4379c874b67d0a6ba7ce12c757b62
Vinod-Jantikar <vinodjantikar999@gmail.com>
2025-01-03 20:40:28 +0530

    solve Equal Number of Occurrences in string

--- index.js
+++ index.js
@@ -416,3 +416,42 @@ var maxProfit = function (prices) {
 
   return maxProfit;
 };
+
+// 1941. Check if All Characters Have Equal Number of Occurrences
+
+// Given a string s, return true if s is a good string, or false otherwise.
+
+// A string s is good if all the characters that appear in s have the same number of occurrences (i.e., the same frequency).
+
+ 
+
+// Example 1:
+
+// Input: s = "abacbc"
+// Output: true
+// Explanation: The characters that appear in s are 'a', 'b', and 'c'. All characters occur 2 times in s.
+// Example 2:
+
+// Input: s = "aaabb"
+// Output: false
+// Explanation: The characters that appear in s are 'a' and 'b'.
+// 'a' occurs 3 times while 'b' occurs 2 times, which is not the same number of times.
+ 
+
+// Constraints:
+
+// 1 <= s.length <= 1000
+// s consists of lowercase English letters.
+
+
+var areOccurrencesEqual = function(s) {
+  const obj = {};
+
+  for(const str of s){
+      obj[str] = (obj[str] || 0) + 1;
+  }
+
+  const frequencies = Object.values(obj);
+
+  return frequencies.every(fre => fre === frequencies[0])
+};
