e904d104651032b3225f4b0997daeb1430950cc8
Vinod-Jantikar <vinodjantikar999@gmail.com>
2025-01-07 23:58:31 +0530

    solve pascal triangle problem

--- mediumArray.js
+++ mediumArray.js
@@ -78,3 +78,41 @@ const combinationSum = (candidates, target) => {
     backtrack(target, [], 0);
     return result
 }
+
+// 119. Pascal's Triangle II
+
+// Given an integer rowIndex, return the rowIndexth (0-indexed) row of the Pascal's triangle.
+// In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+
+// Example 1:
+
+// Input: rowIndex = 3
+// Output: [1,3,3,1]
+// Example 2:
+
+// Input: rowIndex = 0
+// Output: [1]
+// Example 3:
+
+// Input: rowIndex = 1
+// Output: [1,1]
+ 
+
+// Constraints:
+
+// 0 <= rowIndex <= 33
+ 
+
+// Follow up: Could you optimize your algorithm to use only O(rowIndex) extra space?
+
+var getRow = function(rowIndex) {
+    let row = [1];  // Initialize the first element as 1
+   for (let i = 1; i <= rowIndex; i++) {
+       row[i] = 1;  // Set the last element of each row to 1
+       // Update elements from the end to avoid overwriting values needed for the next computation
+       for (let j = i - 1; j > 0; j--) {
+           row[j] = row[j] + row[j - 1];  // Update current element based on the sum of two above
+       }
+   }
+   return row;
+};
